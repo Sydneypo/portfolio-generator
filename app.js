f08346f7eb34575d67f7f4d341e0e8bd7eeb8387
Murphy <sydneyporter74@yahoo.com>
2020-10-02 14:01:24 -0400

    generate all HTML

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer');
 const { AnimationFrameScheduler } = require('rxjs/internal/scheduler/AnimationFrameScheduler');
 
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -135,22 +138,20 @@ if (!portfolioData.projects) {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('Portfolio complete! Check out index.html to see the output!');
+        });
     });
     
     
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
 
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
 
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 
 
 
